915c1ce671412394dde9018d4b87dd7cead07f53
Lars <lars.vlyminckx@hotmail.com>
2018-08-20 15:01:42 +0200

    saven naar 2de database

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -16,12 +16,48 @@ angular.module('pokemonApp', ['ngRoute'])
 .controller("pokemonCtrl", function($scope, pokemonSrv, saveService){
 	pokemonSrv.getAllPokemons().then(function(data){
 		for (var i = 0; i < data.length; i++) {
-			console.log(data[i]);
-			saveService.getObject(data[i].name)
-		}
-		
-		
+			var pokemonName = data[i].name;
+			var pokemon = data[i];
+			saveService.setObject(data[i].name, data[i]);
+		}		
 	});	
+	
+	$("#getInfo").on("click", function(e){
+		var startDate = $("#startDate").val().toLowerCase();
+		var endDate = $("#endDate").val().toLowerCase();
+		
+		console.log(startDate);
+		console.log(endDate);
+		
+		/*saveService.getObject(name).then(function(data){
+			$scope.naam = name; 
+			$scope.image = data.image;
+			$scope.description = data.description;
+			$scope.types = data.types;
+			$scope.status = "This pokemon was already in the database."
+			
+		}, function(err){
+			pokemonService.getAllPokemons().then(function(data){
+				for(var i = 0; data.length; i++){
+					if(data[i].name.toLowerCase() == name){
+						var doc = {};
+						doc.image = data[i].image;
+						doc.description = data[i].description;
+						doc.types = data[i].types;
+						
+						saveService.setObject(name, doc);
+						
+						$scope.naam = name; 
+						$scope.image = data[i].image;
+						$scope.description = data[i].description;
+						$scope.types = data[i].types;
+						$scope.status = "This pokemon is added to the database."
+					};
+				};
+			});
+		});*/
+	});
+	
 })
 
 .service('pokemonSrv', function($http, $q){
