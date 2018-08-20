d8f077ea2a06725803cf52dc424d2298af2b94f5
Lars <lars.vlyminckx@hotmail.com>
2018-08-20 15:39:34 +0200

    Get all pokemons between 2 dates

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -14,10 +14,8 @@ angular.module('pokemonApp', ['ngRoute'])
 })
 
 .controller("pokemonCtrl", function($scope, pokemonSrv, saveService){
-	pokemonSrv.getAllPokemons().then(function(data){
+	pokemonSrv.saveAllPokemonsToNewDatabase().then(function(data){
 		for (var i = 0; i < data.length; i++) {
-			var pokemonName = data[i].name;
-			var pokemon = data[i];
 			saveService.setObject(data[i].name, data[i]);
 		}		
 	});	
@@ -26,47 +24,38 @@ angular.module('pokemonApp', ['ngRoute'])
 		var startDate = $("#startDate").val().toLowerCase();
 		var endDate = $("#endDate").val().toLowerCase();
 		
-		console.log(startDate);
-		console.log(endDate);
-		
-		/*saveService.getObject(name).then(function(data){
-			$scope.naam = name; 
-			$scope.image = data.image;
-			$scope.description = data.description;
-			$scope.types = data.types;
-			$scope.status = "This pokemon was already in the database."
-			
-		}, function(err){
-			pokemonService.getAllPokemons().then(function(data){
-				for(var i = 0; data.length; i++){
-					if(data[i].name.toLowerCase() == name){
-						var doc = {};
-						doc.image = data[i].image;
-						doc.description = data[i].description;
-						doc.types = data[i].types;
-						
-						saveService.setObject(name, doc);
-						
-						$scope.naam = name; 
-						$scope.image = data[i].image;
-						$scope.description = data[i].description;
-						$scope.types = data[i].types;
-						$scope.status = "This pokemon is added to the database."
-					};
-				};
-			});
-		});*/
+		pokemonSrv.getAllDocs().then(function(data){
+			for (var i = 0; i < data.length - 1; i++) {
+				saveService.getObject(data[i].id).then(function(data){
+					if (data.owned > startDate && data.owned < endDate) {		
+						console.log(data.name);
+					}
+				});
+			}
+		});
 	});
 	
 })
 
 .service('pokemonSrv', function($http, $q){
-	this.getAllPokemons = function(){
+	this.saveAllPokemonsToNewDatabase = function(){
 		var q = $q.defer();
 		var url = 'http://127.0.0.1:5984/pokemon1/aa22c07fd07281e7bafa3147f40010fe/pokemon.json'
 		
 		$http.get(url).then(function(data){
 			q.resolve(data.data.docs);
+		}, function(err){
+			q.reject(err);
+		});	
+		
+		return q.promise;
+	};
+	this.getAllDocs = function(){
+		var q = $q.defer();
+		var url = '../../_all_docs';
+		
+		$http.get(url).then(function(data){
+			q.resolve(data.data.rows);
 		}, function(err){
 			q.reject(err);
 		});	
